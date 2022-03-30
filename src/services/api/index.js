470b42eb2382f8dabe7e63ef44943d89b6db2b98
Terry Jung <100718033+terryjung0244@users.noreply.github.com>
2022-03-29 21:33:41 -0400

    getWeatherApi

--- src/services/api/index.js
+++ src/services/api/index.js
@@ -11,3 +11,13 @@ const fetchFunc = (url, method, body) => {
   return apiResult;
 } ;
 
+export const getWeatherApi = async (param) => {
+  let url = `https://api.weatherbit.io/v2.0/current?city=${param.cityName}&country=${param.country}&key=d46fe6f3b2e44fb8b52873c21312b71f`;
+  
+  try {
+    let result = await fetchFunc(url, 'GET', null);
+  } catch (err) {
+    return result
+  }
+}
+
